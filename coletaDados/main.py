ffe8bed354e0fed1e9ab6b87d576f051b88ddf41
Gabriel Yudi <g.sanefugi@gmail.com>
2020-05-14 21:02:49 -0300

    Melhoria na coleta de dados + dados zip
    Melhoria nas falhas nas coleta de dados
    
    Inclui arquivo com 30 dados de cada categoria, sem nenhuma imagem repetida

--- coletaDados/main.py
+++ coletaDados/main.py
@@ -20,26 +20,28 @@ switcher = {
 web = webdriver.Chrome('chromedriver.exe')
 web.get('http://apps.harvardartmuseums.org/art-explorer/')
 web.implicitly_wait(30) # Carrega o site
-web.minimize_window_window()
 
 time.sleep(5) # Espera
 web.find_element_by_id('sets-button').click()
 
 for obj in range(9,17): # Troca a classificação
-    print("/n"+switcher.get(obj))
-    for count in range(0,10): # Quantidade de imagem por classificação
+    print(switcher.get(obj))
+    for count in range(0,30): # Quantidade de imagem por classificação
 
         NaoTemImagem = True  
         while(NaoTemImagem): # apenas troca se a imagem não estava vazia.
-            web.find_elements_by_class_name('interest-button')[obj].click()
-            time.sleep(1) # Garantir que a imagem foi trocado
+            try:
+                web.find_elements_by_class_name('interest-button')[obj].click()
+                time.sleep(2) # Garantir que a imagem foi trocado
 
-            img = web.find_element_by_xpath('//div[@id="random-set"]//img')
-            src = img.get_attribute('src')
+                img = web.find_element_by_xpath('//div[@id="random-set"]//img')
+                src = img.get_attribute('src')
 
-            NaoTemImagem = (src == "http://apps.harvardartmuseums.org/art-explorer/img/no_image.png")
+                NaoTemImagem = (src == "http://apps.harvardartmuseums.org/art-explorer/img/no_image.png")
 
-            if(not NaoTemImagem): # A imagem foi encontrado?
-                wget.download(src, local + switcher.get(obj) +'/'+(str(count+1))+'.png')
+                if(not NaoTemImagem): # A imagem foi encontrado?
+                    wget.download(src, local + switcher.get(obj) +'/'+(str(count+1))+'.png')
+            except:
+                NaoTemImagem = True 
             
 web.quit()
