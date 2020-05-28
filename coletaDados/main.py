0d92dbf0825bbd855bab48e4ac7ba49251f05b77
MrYudi <36682691+MrYudi@users.noreply.github.com>
2020-05-28 17:35:53 -0300

    Nova amostra de dados + melhoria na coleta de dados

--- coletaDados/main.py
+++ coletaDados/main.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 import wget
 
-local = 'C:/Users/Yudi-PC/Desktop/coletaDados/dados/' # Esse caminho precisa ser direto
+local = 'coletaDados/dados/'
 
 switcher = {
     9: "COINS",
@@ -17,16 +17,16 @@ switcher = {
     16: "ARCHIVAL_MATERIAL"
 }
 
-web = webdriver.Chrome('chromedriver.exe')
+web = webdriver.Chrome('coletaDados/chromedriver.exe')
 web.get('http://apps.harvardartmuseums.org/art-explorer/')
 web.implicitly_wait(30) # Carrega o site
 
 time.sleep(5) # Espera
 web.find_element_by_id('sets-button').click()
 
-for obj in range(9,17): # Troca a classificação
+for obj in [9,10,11,14,15]: # Troca a classificação
     print(switcher.get(obj))
-    for count in range(0,30): # Quantidade de imagem por classificação
+    for count in range(0,201): # Quantidade de imagem por classificação
 
         NaoTemImagem = True  
         while(NaoTemImagem): # apenas troca se a imagem não estava vazia.
@@ -43,5 +43,4 @@ for obj in range(9,17): # Troca a classificação
                     wget.download(src, local + switcher.get(obj) +'/'+(str(count+1))+'.png')
             except:
                 NaoTemImagem = True 
-            
 web.quit()
